d3430fe151ed168bc87b398b3347dbae7ee5e883
Robert <robert151196@gmail.com>
2021-01-15 01:59:39 -0300

    Deletes template project names

--- src/modules/views/ProductCategories.js
+++ src/modules/views/ProductCategories.js
@@ -89,57 +89,48 @@ function ProductCategories(props) {
 
   const images = [
     {
-      url:
-        'https://images.unsplash.com/photo-1534081333815-ae5019106622?auto=format&fit=crop&w=400&q=80',
-      title: 'Snorkeling',
+      url: '',
+      title: 'Project',
       width: '40%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1531299204812-e6d44d9a185c?auto=format&fit=crop&w=400&q=80',
-      title: 'Massage',
+      url: '',
+      title: 'Project',
       width: '20%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?auto=format&fit=crop&w=400&q=80',
-      title: 'Hiking',
+      url: '',
+      title: 'Project',
       width: '40%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1453747063559-36695c8771bd?auto=format&fit=crop&w=400&q=80',
-      title: 'Tour',
+      url: '',
+      title: 'Project',
       width: '38%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1523309996740-d5315f9cc28b?auto=format&fit=crop&w=400&q=80',
-      title: 'Gastronomy',
+      url: '',
+      title: 'Project',
       width: '38%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?auto=format&fit=crop&w=400&q=80',
-      title: 'Shopping',
+      url: '',
+      title: 'Project',
       width: '24%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1506941433945-99a2aa4bd50a?auto=format&fit=crop&w=400&q=80',
-      title: 'Walking',
+      url: '',
+      title: 'Project',
       width: '40%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1533727937480-da3a97967e95?auto=format&fit=crop&w=400&q=80',
-      title: 'Fitness',
+      url: '',
+      title: 'Project',
       width: '20%',
     },
     {
-      url:
-        'https://images.unsplash.com/photo-1518136247453-74e7b5265980?auto=format&fit=crop&w=400&q=80',
-      title: 'Reading',
+      url: '',
+      title: 'Project',
       width: '40%',
     },
   ];
